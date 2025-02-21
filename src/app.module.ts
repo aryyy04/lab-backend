347c5a4901695d30d9f8548297c3966570556a73
Alryadi Asmu'Adzan <105841114022@student.unismuh.ac.id>
2025-02-21 14:08:02 +0800

    menambah semua file ke dalam git

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import PrismaService from './prisma';
+import { JwtModule } from '@nestjs/jwt';
+import { ProfileModule } from './profile/profile.module';
+import { MahasiswaProfileModule } from './mahasiswa-profile/mahasiswa-profile.module';
+import { ChatModule } from './chat/chat.module';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      secret: "sdfsghttyu45etgerte4r34fe"
+    }),
+    ProfileModule,
+    MahasiswaProfileModule,
+    ChatModule],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
+})
+export class AppModule {}
